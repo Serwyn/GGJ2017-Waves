98701752dabc4db2a534becd99a7d2bf1272f6d2
Serwyn <sim-guilloud@bluewin.ch>
2017-01-20 22:06:04 +0100

    taking

---
+++ New Unity Project/Assets/Script c#/PlayerScript/TakingDamages.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakingDamages : MonoBehaviour
+{
+
+    PlayerState ps4;
+    public float highWaveDamages;
+
+	// Use this for initialization
+	void Start () {
+        ps4 = this.GetComponent<PlayerState>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "onde")
+        {
+            ps4.life -= highWaveDamages;
+        }
+
+    }
+}
