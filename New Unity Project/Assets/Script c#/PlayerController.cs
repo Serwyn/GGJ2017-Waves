8ff29c632d84b9a31da8159fd97c5f9fcb3ff237
Tutifruiti <maxime.jaquier@epfl.ch>
2017-01-20 19:43:55 +0100

    player script move

---
+++ New Unity Project/Assets/Script c#/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public int moveSpeed;
+    public int jumpForce;
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton("HorizontalMove"))
+        {
+            HorizontalMove();
+        }
+
+        if (Input.GetButtonDown("jump"))
+        {
+            jump();
+        }
+
+        if (Input.GetButtonDown("crouch"))
+        {
+            crouch();
+        }
+	}
+
+    public void HorizontalMove()
+    {
+        
+    }
+
+    public void jump()
+    {
+
+    }
+
+    public void crouch()
+    {
+
+    }
+
+}
