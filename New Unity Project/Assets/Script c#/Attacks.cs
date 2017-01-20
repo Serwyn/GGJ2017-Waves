a2ef56b9028526a784df446c2e460587d139c1d5
Serwyn <sim-guilloud@bluewin.ch>
2017-01-20 20:58:51 +0100

    attacks

---
+++ New Unity Project/Assets/Script c#/Attacks.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacks : MonoBehaviour {
+
+    public GameObject onde;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void highAttack()
+    {
+        GameObject attack = Instantiate(onde, transform.position, Quaternion.identity);
+        Rigidbody2D aRB = attack.GetComponent<Rigidbody2D>();
+        aRB.velocity = new Vector2(1, 0);
+    }
+
+
+
+
+}
---
+++ New Unity Project/Assets/Script c#/highWave.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class highWave : MonoBehaviour {
+
+	// Use this for initialization
+    private float lifespan = 4.0f;
+	void Start () {
+        Destroy(this, lifespan);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
