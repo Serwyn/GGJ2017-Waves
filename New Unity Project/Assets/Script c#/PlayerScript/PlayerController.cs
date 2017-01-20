4213008b2ef4cae683c7084b076ec6af9c6b6742
Tutifruiti <maxime.jaquier@epfl.ch>
2017-01-20 21:40:09 +0100

    a

---
+++ New Unity Project/Assets/Script c#/PlayerScript/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+    public float maxSpeed;
+    Rigidbody2D rb;
+    PlayerState playerState;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        playerState = GetComponent<PlayerState>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("HorizontalMove"))
+        {
+            horizontalMove();
+        }
+        else
+        {
+            //stop move
+        };
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump();
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch();
+        }
+    }
+
+    public void horizontalMove()
+    {
+        rb.velocity =new Vector2(0, rb.velocity.y) + Vector2.right * Input.GetAxisRaw("HorizontalMove") * moveSpeed;
+    }
+
+    public void jump()
+    {
+        if (playerState.isLanded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0) + Vector2.up * jumpForce;
+            playerState.isLanded = false;
+        }
+    }
+
+    public void crouch()
+    {
+
+    }
+
+}
---
+++ New Unity Project/Assets/Script c#/PlayerScript/PlayerState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour {
+
+    public bool isLanded;
+    public bool hasJump;
+    public bool isOnWall;
+
+	// Use this for initialization
+	void Start () {
+        isLanded = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Ground")
+        {
+            isLanded = true;
+        }
+    }
+}
