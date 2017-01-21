af2c89f684ca23be0dd0d9588fc2a3fded49d597
Aolledin <mathias.bavarel@gmail.com>
2017-01-21 14:55:50 +0100

    Life bar jolie

--- New Unity Project/Assets/Script c#/UIScripts/LifeScript.cs
+++ New Unity Project/Assets/Script c#/UIScripts/LifeScript.cs
@@ -23,9 +23,7 @@ public class LifeScript : MonoBehaviour {
 	void Update () {
 
         UpdateLifeBar();
-    
-
-    
+ 
 
     }
 
@@ -36,8 +34,20 @@ public class LifeScript : MonoBehaviour {
         float life1 = p1.GetComponent<PlayerState>().life;
         float life2 = p2.GetComponent<PlayerState>().life;
 
-        lb1.GetComponent<Image>().fillAmount = life1 / MAXLIFE;
-        lb2.GetComponent<Image>().fillAmount = life2 / MAXLIFE;
+        Image im1 = lb1.GetComponent<Image>();
+        Image im2 = lb2.GetComponent<Image>();
+
+        im1.fillAmount = life1 / MAXLIFE; //On change le remplissage
+        im2.fillAmount = life2 / MAXLIFE;
+
+
+        //On change la couleur
+        Color couleur1 = new Color(1 - life1 / MAXLIFE, life1 / MAXLIFE, 0, 1);
+        im1.color = couleur1;
+  
+        Color couleur2 = new Color(1 - life2 / MAXLIFE, life2 / MAXLIFE, 0, 1);
+        im2.color = couleur2;
+
 
 
     }
