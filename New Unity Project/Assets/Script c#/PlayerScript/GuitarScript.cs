464f1fa182456475b1aba1a405eb742c4d4b3ae8
Tutifruiti <maxime.jaquier@epfl.ch>
2017-01-21 19:26:47 +0100

    ertxdfcz

---
+++ New Unity Project/Assets/Script c#/PlayerScript/GuitarScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuitarScript : MonoBehaviour {
+
+    public GameObject list;
+    public List<AudioClip> guit;
+    public int guitNbr;
+
+	// Use this for initialization
+	void Start () {
+        guitNbr = 0;
+        guit = list.GetComponent<soundList>().rock;
+	}
+	
+    public AudioClip getGuitSound()
+    {
+        AudioClip sound = guit[guitNbr % guit.Count];
+        return sound;
+    }
+
+
+}
---
+++ New Unity Project/Assets/Script c#/PlayerScript/TrumpScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrumpScript : MonoBehaviour {
+
+    public GameObject list;
+    public List<AudioClip> trump;
+    public int trumpNbr;
+
+	// Use this for initialization
+	void Start () {
+        trumpNbr = 0;
+        trump = list.GetComponent<soundList>().trump;
+	}
+	
+    public AudioClip getTrumpSound()
+    {
+        AudioClip sound = trump[trumpNbr % trump.Count];
+        return sound;
+    }
+
+
+}
