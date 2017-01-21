368cfa3da98b7c2b9e6e5c698b0eca9c2014a2c5
Tutifruiti <maxime.jaquier@epfl.ch>
2017-01-21 04:06:20 +0100

    truc trop louch

---
+++ New Unity Project/Assets/Script c#/UIScripts/StartMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenuScript : MonoBehaviour {
+
+    public GameObject mainPanel;
+    public GameObject selectorPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartButton()
+    {
+        mainPanel.SetActive(false) ;
+        selectorPanel.SetActive(true);
+
+    }
+    public void BackButton()
+    {
+        mainPanel.SetActive(true);
+        selectorPanel.SetActive(false);
+    }
+
+}
