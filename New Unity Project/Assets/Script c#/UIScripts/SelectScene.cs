17c5b299dbcc9e6dc9fd97c6ee78ae2254cf84c4
Tutifruiti <maxime.jaquier@epfl.ch>
2017-01-22 02:32:20 +0100

    hey

---
+++ New Unity Project/Assets/Script c#/UIScripts/SelectScene.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectScene : MonoBehaviour {
+
+    public GameObject scenePrewiev;
+    public List<string> sceneName;
+    public List<Sprite> preview;
+    public int sceneNbr;
+
+	// Use this for initialization
+	void Start () {
+        sceneNbr = 0;
+	}
+	
+    public void leftArrow()
+    {
+        sceneNbr -= 1;
+        updateScene();
+    }
+    public void rightArrow()
+    {
+        sceneNbr += 1;
+        updateScene();
+    }
+
+    public void updateScene()
+    {
+        scenePrewiev.GetComponent<Image>().sprite = preview[sceneNbr % preview.Count];
+    }
+
+    public string getSceneName()
+    {
+        string name = sceneName[sceneNbr % sceneName.Count];
+        return name;
+    }
+	// Update is called once per frame
+	
+}
