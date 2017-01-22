9c55352df831ff2092481ac9c2fddb35e187054c
Tutifruiti <maxime.jaquier@epfl.ch>
2017-01-22 05:48:03 +0100

    pushoubilou

---
+++ New Unity Project/Assets/Script c#/UIScripts/SuperScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SuperScript : MonoBehaviour {
+
+    public float fillUp;
+    public float maxFill;
+
+	// Use this for initialization
+	void Start () {
+        fillUp = 0;
+        GetComponent<Image>().fillAmount = fillUp / maxFill;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setFill(int amount)
+    {
+        fillUp += amount;
+        GetComponent<Image>().fillAmount = fillUp/maxFill;
+        if(fillUp == maxFill)
+        {
+            //set super as true
+        }
+    }
+
+}
